377e4d6de9d7e0d402a3708c99dd1ad3e106db92
SquareFong <fanghaocheng2008@126.com>
2018-04-17 22:51:34 +0800

    finishing load data

--- KNN/HandwritingRecognition.h
+++ KNN/HandwritingRecognition.h
@@ -5,4 +5,55 @@
 #ifndef KNN_HANDWRITINGRECOGNITION_H
 #define KNN_HANDWRITINGRECOGNITION_H
 
+#include <fstream>
+#include <vector>
+#include <string>
+#include <dirent.h>
+#include <algorithm>
+using namespace std;
+
+vector<string> getFiles(const string &directory)
+{
+    vector<string> files;//存放文件名
+    DIR *dir= nullptr;
+    struct dirent *entry;
+    char path[1000];
+    if ((dir=opendir(directory.c_str())) == nullptr)
+    {
+        perror("Open dir error...");
+        exit(1);
+    }
+
+    while ((entry = readdir(dir)) != nullptr) {
+        if(entry->d_name[0] != '.'){
+            files.emplace_back(entry->d_name);
+        }
+    }
+
+    closedir(dir);
+    sort(files.begin(),files.end());
+    return files;
+}
+
+vector<vector<int>> loadData(string directory){
+    vector<string> files = getFiles(directory);
+    if(directory[directory.length()-1] != '/'){
+        directory += '/';
+    }
+    vector<vector<int>> data;
+    for(auto &file : files){
+        vector<int> temp;
+        ifstream infile(directory+file);
+        char c;
+        while(infile.get(c)){
+            if(c == '\n')
+                continue;
+            temp.emplace_back(c);
+        }
+        data.push_back(temp);
+        infile.close();
+    }
+    return data;
+}
+
 #endif //KNN_HANDWRITINGRECOGNITION_H
--- KNN/main.cpp
+++ KNN/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "HandwritingRecognition.h"
 
 int main() {
+    auto files =
+            getFiles("/home/squarefong/Documents/PatternRecgnitionPractice/KNN/trainingDigits");
+    auto d=loadData("/home/squarefong/Documents/PatternRecgnitionPractice/KNN/trainingDigits");
+    for (int i = 0; i < d.size(); ++i) {
+        for (int j = 0; j < d[0].size(); ++j) {
+            cout << char(d[i][j]) << ' ';
+        }
+        cout << endl;
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
