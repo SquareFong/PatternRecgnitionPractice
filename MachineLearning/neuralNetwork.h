618cb015a9c69fb58794fe6fa0c6cfe0902cc7c3
SquareFong <fanghaocheng2008@126.com>
2018-04-18 20:47:28 +0800

    finish single Neuron

---
+++ MachineLearning/neuralNetwork.h
@@ -0,0 +1,54 @@
+//
+// Created by squarefong on 18-4-18.
+//
+
+#ifndef MACHINELEARNING_NEURALNETWORK_H
+#define MACHINELEARNING_NEURALNETWORK_H
+
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+//自然常数（Natural constant）
+const double e=2.71828182845904523536
+
+class ArtificialNeuron{
+    //权重
+    vector<double> weight;
+    //阈值
+    double threshold;
+    double scalarProduct(const vector<double> &x){
+        if(x.size()!=weight.size())
+        {
+            exit(1);
+        }
+        double result(0.0);
+        for(int i=0; i<x.size(); ++i){
+            result += x[i] * weight[i];
+        }
+        return result;
+    }
+    double activationFunction(double x){
+        return 1 / (1 + pow(e,x));
+    }
+public:
+    void setWeight(const vector<double> &Weight){
+        weight = Weight;
+    }
+    bool setWeight(const unsigned &sub, const double &value){
+        if (sub >= weight.size())
+            return false;
+        weight[sub] = value;
+        return true;
+    }
+    void setThreshold(const double &Threshold){
+        threshold = Threshold;
+    }
+    double output(const vector<double> &x){
+        double z = scalarProduct(x) + threshold;
+        return activationFunction(scalarProduct(x) + threshold);
+    }
+};
+
+
+#endif //MACHINELEARNING_NEURALNETWORK_H
